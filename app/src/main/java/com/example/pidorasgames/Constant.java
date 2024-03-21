b0da4d102862b45dd99cc8d22661ccad875bec5e
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2024-03-21 13:54:12 +0700

    Perenos -ver 2.0

---
+++ app/src/main/java/com/example/pidorasgames/Constant.java
@@ -0,0 +1,8 @@
+package com.example.pidorasgames;
+
+public class Constant {
+    public final static String USER_NAME = "user_name";
+    public final static String USER_SUB_NAME = "user_sub_name";
+    public final static String USER_GMAIL = "user_gmail";
+    public final static int USER_ID_NUMB = 0;
+}
